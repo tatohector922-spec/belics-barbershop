e713183d7ce991eeb823cb5261c77cc6f34ffab3
tatohector922-spec <tatohector922@gmail.com>
2026-08-30 12:36:11 -0700

    Corregir configuracion de prisma7

--- prisma7.config.ts
+++ prisma7.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from '@prisma/config';
 
 export default defineConfig({
-  earlyAccess: true,
   datasource: {
-    url: "postgresql://postgres.lhtxvemwfjutxgofyeoc:Tatofugeroa12%40@aws-0-us-west-2.pooler.supabase.com:6543/postgres",
+    url: process.env.DATABASE_URL,
   },
 });
